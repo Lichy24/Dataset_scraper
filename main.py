b5161a23c5a29e4fca66c40166833530a9d58a6f
Adi Lichy <adi_lichy@yahoo.com>
2023-10-03 15:47:09 +0300

    Update main.py
    update main to allow in MTA to select the years to scrap from

--- main.py
+++ main.py
@@ -33,6 +33,7 @@ Original source:
 
 from stratoshpereips_extractor import Straoshpereips
 from MTA_extractor import MTA
+from datetime import datetime
 
 save_dir = './pcap_zip'
 extract_dir = './extracted'
@@ -43,14 +44,18 @@ print('The script support 2 datasets webpages:\n\t1) Malware Traffic Analysis\n\
 ans = input('Please selected the wanted dataset webpage to be pulled by inputing the number....')
 if ans == '1':
     ans = input('Please Selected which page to extract:\n\t1) exercises\n\t2) blogs\n\t3) both....')
+    current_year = datetime.today().year
+    ans_years = input(f'Please type from what years you want the dataset from: 2013-{current_year}')
+    year_start , year_end = ans_years.split('-')
+    years = [*range(int(year_start), int(year_end)+1)]
     mta = MTA(save_dir,extract_dir)
     if ans == '1':
-        mta.MTA_exercises_or_blogs('exercises')
+        mta.MTA_exercises_or_blogs('exercises', years)
     elif ans == '2':
-        mta.MTA_exercises_or_blogs('blogs')
+        mta.MTA_exercises_or_blogs('blogs', years)
     elif ans == '3':
-        mta.MTA_exercises_or_blogs('blogs')
-        mta.MTA_exercises_or_blogs('exercises')
+        mta.MTA_exercises_or_blogs('blogs', years)
+        mta.MTA_exercises_or_blogs('exercises', years)
 elif ans == '2':
     print("The Stratosphereips download the malware page:(can also be used for the bengin page not been tested)")
     #ans = input("Please enter which page to extract:(Works for Malware and Bengin)....")
